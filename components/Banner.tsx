c962c8bde3efef7b441bec80fbb35280a577085f
Gaku Ishikawa <gaku.jp.301@gmail.com>
2025-05-09 15:28:41 +0900

    install expo-insights and change add unitId

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,8 +1,8 @@
 import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
 
 export default function Banner() {
-  const unitId = TestIds.BANNER;
-  // const unitId = 'ca-app-pub-4739003769773423/5429217778';
+  // const unitId = TestIds.BANNER;
+  const unitId = 'ca-app-pub-4739003769773423/5429217778';
   return (
     <BannerAd
       // ios
